9b37ca5fcf71f57d81db5df6c6e3ba15c0b37055
Aneesh Agrawal <aneeshusa@gmail.com>
2014-01-18 23:21:37 -0500

    Changed settings, cleaned up files

--- android/src/com/postpushr/fragments/RegisterFragment.java
+++ android/src/com/postpushr/fragments/RegisterFragment.java
@@ -11,8 +11,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.postpushr.R;
-import com.postpushr.R.id;
-import com.postpushr.R.layout;
 import com.stripe.android.Stripe;
 import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Card;
@@ -28,11 +26,13 @@ public class RegisterFragment extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_register, parent, false);
 
-		Button registerSubmitButton = (Button) view.findViewById(R.id.register_submit_button);
-		registerSubmitButton.setOnClickListener(new RegisterSubmitListener()); 
+		Button registerSubmitButton = (Button) view
+				.findViewById(R.id.register_submit_button);
+		registerSubmitButton.setOnClickListener(new RegisterSubmitListener());
 
 		return view;
 	}
@@ -42,49 +42,57 @@ public class RegisterFragment extends Fragment {
 		public void onClick(final View view) {
 			// TODO Validation
 
-			String creditCardNumber = ((EditText) getView().findViewById(R.id.register_credit_card_number_edittext)).getText().toString();
-			int creditCardExpirationMonth = Integer.parseInt(((EditText) getView().findViewById(R.id.register_credit_card_expiration_date_month_edittext)).getText().toString());
-			int creditCardExpirationYear = Integer.parseInt(((EditText) getView().findViewById(R.id.register_credit_card_expiration_date_year_edittext)).getText().toString());
-			String creditCardSecurityCode = ((EditText) getView().findViewById(R.id.register_credit_card_security_code_edittext)).getText().toString();
+			String creditCardNumber = ((EditText) getView().findViewById(
+					R.id.register_credit_card_number_edittext)).getText()
+					.toString();
+			int creditCardExpirationMonth = Integer
+					.parseInt(((EditText) getView()
+							.findViewById(
+									R.id.register_credit_card_expiration_date_month_edittext))
+							.getText().toString());
+			int creditCardExpirationYear = Integer
+					.parseInt(((EditText) getView()
+							.findViewById(
+									R.id.register_credit_card_expiration_date_year_edittext))
+							.getText().toString());
+			String creditCardSecurityCode = ((EditText) getView().findViewById(
+					R.id.register_credit_card_security_code_edittext))
+					.getText().toString();
 
-			Card card = new Card(creditCardNumber, creditCardExpirationMonth, creditCardExpirationYear, creditCardSecurityCode);
+			Card card = new Card(creditCardNumber, creditCardExpirationMonth,
+					creditCardExpirationYear, creditCardSecurityCode);
 
 			if (card.validateNumber() && card.validateCVC()) {
 
 				Stripe stripe;
 				try {
 					stripe = new Stripe("pk_test_bRZoGvGajV5TeFq28CmFyxYa");
-					stripe.createToken(
-							card,
-							new TokenCallback() {
-								public void onSuccess(Token token) {
-									// Send token to your server
-									Toast.makeText(view.getContext(),
-											"Made it werk. Now make it twerk",
-											Toast.LENGTH_LONG
-											).show();
-								}
-								public void onError(Exception error) {
-									// Show localized error message
-									Toast.makeText(view.getContext(),
-											error.getLocalizedMessage(),
-											Toast.LENGTH_LONG
-											).show();
-								}
-							}
-							);
+					stripe.createToken(card, new TokenCallback() {
+						@Override
+						public void onSuccess(Token token) {
+							// Send token to your server
+							Toast.makeText(view.getContext(),
+									"Made it werk. Now make it twerk",
+									Toast.LENGTH_LONG).show();
+						}
+
+						@Override
+						public void onError(Exception error) {
+							// Show localized error message
+							Toast.makeText(view.getContext(),
+									error.getLocalizedMessage(),
+									Toast.LENGTH_LONG).show();
+						}
+					});
 				} catch (AuthenticationException e) {
-					Toast.makeText(view.getContext(),
-							"auth error",
-							Toast.LENGTH_LONG
-							).show();
+					Toast.makeText(view.getContext(), "auth error",
+							Toast.LENGTH_LONG).show();
 				}
 
 			} else {
 				Toast.makeText(view.getContext(),
-						"card didn't validate/check cvc",
-						Toast.LENGTH_LONG
-						).show();
+						"card didn't validate/check cvc", Toast.LENGTH_LONG)
+						.show();
 			}
 		}
 	}
--- android/src/com/postpushr/model/Order.java
+++ android/src/com/postpushr/model/Order.java
@@ -1,6 +1,8 @@
 package com.postpushr.model;
 
 import java.text.DateFormat;
+
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 
 public class Order {
@@ -9,31 +11,32 @@ public class Order {
 	private final Bitmap mPicture;
 	private final Recipient[] mRecipients;
 	private final float mPrice;
-	
-	public Order(DateFormat orderDate, Bitmap picture, Recipient[] recipients, float price) {
+
+	public Order(DateFormat orderDate, Bitmap picture, Recipient[] recipients,
+			float price) {
 		mOrderDate = orderDate;
 		mPicture = picture;
 		mRecipients = recipients;
 		mPrice = price;
 	}
-	
+
 	public String getOrderDateString() {
 		// TODO test this formatting
 		return DateFormat.getDateInstance(DateFormat.LONG).format(mOrderDate);
 	}
-	
+
 	public Bitmap getPictureBitmap() {
 		return mPicture;
 	}
-	
+
 	public Recipient[] getRecipients() {
 		return mRecipients;
 	}
-	
+
+	@SuppressLint("DefaultLocale")
 	public String getPriceString() {
 		// TODO test this formatting
 		return String.format("%.2f", mPrice);
 	}
-	
-	
+
 }
